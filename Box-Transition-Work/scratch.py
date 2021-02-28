295ad3ec66c02ce370e3c8ece3327e6b7acf91e6
Elisa Beshero-Bondar <ebbondar@gmail.com>
2021-02-27 20:23:33 -0500

    scratch file to write new directory and file inside

--- Box-Transition-Work/scratch.py
+++ Box-Transition-Work/scratch.py
@@ -2,6 +2,12 @@ from pathlib import Path
 import os
 from os import mkdir
 projectPath = Path('E:/Users/Lisa Documents/Documents/GitHub/Digital-Mitford-Organization/DM_documentation/Box-Transition-Work')
-var = "elisa"
-newPath = os.path.join(projectPath, var)
-mkdir(newPath)
+newFolder = "elisa"
+commentFile = "comment.txt"
+Path(projectPath/newFolder).mkdir()
+newPath = os.path.join(projectPath, newFolder, commentFile)
+commentFile = open(newPath, 'w')
+contents = 'Testing to add content to this file.'
+commentFile.write(contents)
+commentFile.close()
+print(commentFile)
