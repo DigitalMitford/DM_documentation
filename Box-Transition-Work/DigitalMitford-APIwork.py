55dab8250ab21dccc07fe0a621d4457ab6f2f8d9
Elisa Beshero-Bondar <ebbondar@gmail.com>
2021-02-28 18:27:24 -0500

    updating Python script, removing ? from dir names

--- Box-Transition-Work/DigitalMitford-APIwork.py
+++ Box-Transition-Work/DigitalMitford-APIwork.py
@@ -1,17 +1,37 @@
 # from typing import List, Any, Tuple
 # import tarfile
+from typing import Any
+
 from boxsdk import OAuth2
 from boxsdk import Client
+from pathlib import Path
+import os
+from os import mkdir
 # Not using the libraries below. Authenticate using the developer token which needs to be updated hourly.
 # from boxsdk import DevelopmentClient
 # from boxsdk import JWTAuth
 # ebb: Try setting this up for JWT auth to use the Developer token https://github.com/box/box-python-sdk#server-to-server-auth-with-jwt
 import requests
 
+projectPath = Path('E:/Users/Lisa Documents/Documents/GitHub/Digital-Mitford-Organization/DM_documentation/Box-Transition-Work')
 
 def recurfolders(getFolder):
+    # ebb: What if instead we filed the description files inside the actual downloaded Box directories that share the same name as the folder.name?
+    # Would we need os.walk for this?
+    # Maybe easiest to post the descriptions as files to Box, and let them sync locally.
+    # See https://github.com/box/box-python-sdk/blob/master/docs/usage/files.md#upload-a-file
     if getFolder.description:
-        print(getFolder.description)
+        foldername = getFolder.name
+        foldernamecorr = foldername.replace('?', '')
+        folderid = getFolder.id
+        labelcontents = getFolder.description
+        labelfilename = foldernamecorr + '----' + folderid + ".txt"
+        newPath = os.path.join(projectPath, labelfilename)
+        labelFile = open(newPath, 'w', encoding='utf-8')
+        labelFile.write(labelcontents)
+        labelFile.close()
+        labelfileupload = client.folder(folderid).upload(newPath)
+        print('File "{0}" uploaded to Box with file ID {1}'.format(labelfileupload.name, labelfileupload.id))
     for i in getFolder.get_items():
         substring = "Folder"
         if substring in str(i):
@@ -27,7 +47,7 @@ def recurfolders(getFolder):
 oauth = OAuth2(
     client_id='37zh1wo00w7h8qphpviwjkia7ng8g1j4',
     client_secret='YKatTFXOH1icNc9uxD3K2TMLCiulQJ0M',
-    access_token='fA9hPCQgAHWjPrt1Jn9TI98ha7ol3wAJ'
+    access_token='AzoJ2PXouN7iOS3Sk0kkIf6GuovPtXn0'
     # store_tokens=your_store_tokens_callback_method,
 )
 
@@ -53,5 +73,7 @@ print('Folder "{0}" has this metadata: "{1}" and this description: "{2}".'.forma
     folder.metadata,
     folder.description
 ))
+MitfordDigitalArchives = client.folder(folder_id='907565446').get()
 lettersOuterFolder = client.folder(folder_id='907769116').get()
-recurfolders(lettersOuterFolder)
+testingOuterFolder = client.folder(folder_id='132569588068').get()
+recurfolders(MitfordDigitalArchives)
